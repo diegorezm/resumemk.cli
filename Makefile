a017e5dee9e03d70cdeac222d4d596911a3a1d0b
diegorezm <dmourarezende@gmail.com>
2025-01-11 21:33:29 -0300

    better cli

--- Makefile
+++ Makefile
@@ -1,12 +1,20 @@
 USER_HOME := $(shell echo ~)
 
 build-client:
-	cd app && bun run build
+	cd apps/server/app && npm ci && npm run build
 
-build-server:
-	cargo build --release
+build-server: build-client
+	cargo build -p resumemk_server --release
 
-install: build-client build-server
-	cp target/release/resumemk $(USER_HOME)/.local/bin/resumemk
+build-cli:
+	cargo build -p resumemk_cli --release
+
+
+install-server: build-server
+	cp target/release/resumemk_server $(USER_HOME)/.local/bin/resumemk_server
+	resumemk_server
+
+install-cli: build-cli
+	cp target/release/resumemk_cli $(USER_HOME)/.local/bin/resumemk
 	resumemk --help
 
--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -5,34 +5,25 @@ use std::{path::PathBuf, str::FromStr};
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct CLI {
-    #[clap(subcommand)]
-    pub command: Command,
-}
+    /// Input file path
+    #[clap(short, long)]
+    input: PathBuf,
 
-#[derive(Parser, Debug)]
-pub enum Command {
-    #[command(about = "Build a resume from a markdown file")]
-    Build {
-        /// Input file path
-        #[clap(short, long)]
-        input: PathBuf,
+    /// Output file path (optional). If not provided, it defaults to the input file name with the specified output type extension.
+    #[clap(short, long)]
+    output: Option<PathBuf>,
 
-        /// Output file path (optional). If not provided, it defaults to the input file name with the specified output type extension.
-        #[clap(short, long)]
-        output: Option<PathBuf>,
+    /// Output type (pdf or html). Defaults to pdf.
+    #[clap(name = "type", short, long, default_value = "pdf")]
+    output_type: OutputType,
 
-        /// Output type (pdf or html). Defaults to pdf.
-        #[clap(name = "type", short, long, default_value = "pdf")]
-        output_type: OutputType,
+    /// Set a custom stylesheet for the output. Defaults to the default stylesheet.
+    #[clap(short, long)]
+    stylesheet: Option<PathBuf>,
 
-        /// Set a custom stylesheet for the output. Defaults to the default stylesheet.
-        #[clap(short, long)]
-        stylesheet: Option<PathBuf>,
-
-        // Set the title of the resume. Defaults to the input file name.
-        #[clap(long)]
-        title: Option<String>,
-    },
+    // Set the title of the resume. Defaults to the input file name.
+    #[clap(long)]
+    title: Option<String>,
 }
 
 #[derive(Debug, Clone)]
@@ -81,53 +72,45 @@ pub fn get_output_path(
 
 fn main() {
     let cli = init_cli();
-    match cli.command {
-        Command::Build {
-            input,
-            output,
-            output_type,
-            stylesheet,
-            title,
-        } => {
-            let output_path = get_output_path(&input, &output, &output_type);
-            let document_title = title.unwrap_or(output_path.to_str().unwrap().to_string());
-            let mut cli_resume_builder = CLIResumeBuilder::new();
+    let output_path = get_output_path(&cli.input, &cli.output, &cli.output_type);
+    let document_title = cli
+        .title
+        .unwrap_or(output_path.to_str().unwrap().to_string());
+    let mut cli_resume_builder = CLIResumeBuilder::new();
 
-            if let Some(stylesheet) = stylesheet {
-                cli_resume_builder.set_stylesheet(stylesheet.as_path());
-            }
+    if let Some(stylesheet) = cli.stylesheet {
+        cli_resume_builder.set_stylesheet(stylesheet.as_path());
+    }
 
-            let pdf_options = headless_chrome::types::PrintToPdfOptions::default();
+    let pdf_options = headless_chrome::types::PrintToPdfOptions::default();
 
-            match output_type {
-                OutputType::HTML => {
-                    match cli_resume_builder.save_to_html(
-                        input.as_path(),
-                        output_path.as_path(),
-                        document_title.as_str(),
-                    ) {
-                        Ok(path) => {
-                            println!("Successfully saved to {}", path);
-                        }
-                        Err(e) => {
-                            println!("Error: {}", e);
-                        }
-                    }
+    match cli.output_type {
+        OutputType::HTML => {
+            match cli_resume_builder.save_to_html(
+                cli.input.as_path(),
+                output_path.as_path(),
+                document_title.as_str(),
+            ) {
+                Ok(path) => {
+                    println!("Successfully saved to {}", path);
                 }
-                OutputType::PDF => {
-                    match cli_resume_builder.save_to_pdf(
-                        input.as_path(),
-                        output_path.as_path(),
-                        pdf_options,
-                        document_title.as_str(),
-                    ) {
-                        Ok(_) => {
-                            println!("Successfully saved to {}", output_path.to_str().unwrap());
-                        }
-                        Err(e) => {
-                            println!("Error: {}", e);
-                        }
-                    }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            }
+        }
+        OutputType::PDF => {
+            match cli_resume_builder.save_to_pdf(
+                cli.input.as_path(),
+                output_path.as_path(),
+                pdf_options,
+                document_title.as_str(),
+            ) {
+                Ok(_) => {
+                    println!("Successfully saved to {}", output_path.to_str().unwrap());
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
                 }
             }
         }
