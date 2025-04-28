1a7169a6bd2bfbe73d5f8b452952c163bc891b05
diegorezm <dmourarezende@gmail.com>
2025-04-28 00:46:25 -0300

    better modal

---
+++ apps/server/web/src/modal.js
@@ -0,0 +1,36 @@
+/**
+ * Sets up a modal with open, close, and backdrop functionalities.
+ * @param {function} [onClose] Function that executes when the modal is closed.
+ */
+function setupModal(onClose) {
+  const openModalBtns = document.querySelectorAll(".open-modal-btn");
+  const closeModalBtns = document.querySelectorAll(".close-modal-btn");
+  const modal = document.getElementById("my_modal");
+
+  if (!openModalBtns || !modal) {
+    console.error("One or more modal elements not found.");
+    return;
+  }
+
+  modal.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (e.target === modal) {
+      modal.classList.add("hidden");
+    }
+  });
+
+  openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.classList.remove("hidden");
+    });
+  });
+
+  closeModalBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.classList.add("hidden");
+      if (onClose) {
+        onClose();
+      }
+    });
+  });
+}
